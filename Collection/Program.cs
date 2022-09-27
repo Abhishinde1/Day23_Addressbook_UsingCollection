9d2a3362827126f188a203ab57e76879a8a325ab
AbhiShinde1 <abhishinde572@gmail.com>
2022-09-27 16:02:35 +0530

    No Duplicate Person Program Added

---
+++ Collection/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Collection
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Address book Program");
+
+            AddressBookCreate createAddressBook = new AddressBookCreate();
+            createAddressBook.ReadInput();
+        }
+    }
+}
